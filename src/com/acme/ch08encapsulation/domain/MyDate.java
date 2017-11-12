a0b48379f8aa88d302c8f5d4ec02c394819c08fd
Marius Druga <druga.marius@gmail.com>
2017-11-13 00:34:50 +0200

    finish lab 8

--- src/com/acme/ch08encapsulation/domain/MyDate.java
+++ src/com/acme/ch08encapsulation/domain/MyDate.java
@@ -1,80 +1,94 @@
 package com.acme.ch08encapsulation.domain;
 
 public class MyDate {
-    private int day;
-    private int year;
-    private int month;
-    int numberOfDays;
+	private int day;
+	private int year;
+	private int month;
+	
+	int numberOfDays;
 
-    public MyDate(){}
-    
+	public MyDate() {
+	}
 
-    public int getDay() {
+	public int getDay() {
 		return day;
 	}
 
-
-
 	public void setDay(int day) {
-		this.day = day;
+		if (valid (day, this.month, this.year)) {
+			this.day = day;
+		}
 	}
-
-
 
 	public int getMonth() {
 		return month;
 	}
 
-
-
 	public void setMonth(int month) {
-		this.month = month;
+		if (valid (this.day, month, this.year)) {
+			this.month = month;
+		}
 	}
-
-
 
 	public void setYear(int year) {
-		this.year = year;
+		if (valid (this.day, this.month, year)) {
+			this.year = year;
+		}
+	}
+	
+	public int getYear() {
+		return this.year;
 	}
 
+	// use the parameters of m, d and y to set the three attributes
+	public MyDate(int d, int m, int y) {
+		if (valid(d, m, y)) {
+			day = d;
+			month = m;
+			year = y;
+		}
+	}
 
+	@Override
+	public String toString() {
+		return this.day + "/" + this.month + "/" + this.year;
+	}
 
-	//use the parameters of m, d and y to set the three attributes
-    public MyDate(int m, int d, int y){
+	public static void leapYears() {
 
-        day = d;
-        month = m;
-        year = y;
-    }
-
-    //return a string with month/day/year like 01/20/1964
-    public String crtYear(){
-        String currentDate = day + "/" + month + "/" + year;
-        return currentDate;
-    }
-
-    //set the MyDate attributes with m, d, and y values here!
-    public void setDate(int m, int d, int y){
-        day = d;
-        month = m;
-        year = y;
-    }
-    
-    public int getYear() {
-    	return this.year;
-    }
-
-    public static void leapYears() {
-    	
-    	for (int i = 1752; i <= 2020; i ++) {
-    		if (i % 4 == 0) {
-    			if (i % 100 != 0) {
-    				System.out.println("The year "+ i + " is a leap year");
-    			}
-    			else if (i % 400 == 0 && i % 100 == 0) {
-    				System.out.println("The year "+ i + " is a leap year");
-    			}
+		for (int i = 1752; i <= 2020; i++) {
+			if (i % 4 == 0) {
+				if (i % 100 != 0) {
+					System.out.println("The year " + i + " is a leap year");
+				} else if (i % 400 == 0 && i % 100 == 0) {
+					System.out.println("The year " + i + " is a leap year");
+				}
 			}
 		}
-    }
+	}
+
+	public boolean valid(int day, int month, int year) {
+		if (day < 1 || day > 31 || month < 1 || month > 12) {
+			System.out.println("Attempting to create a non-valid date: " + day + "/" + month + "/" + year);
+			return false;
+		}
+		switch (month) {
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				return day <= 30;
+			case 2: {
+				if (day <= 28) {
+					return true;
+				} else if (day == 29 && year % 4 == 0) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
--- src/com/acme/ch08encapsulation/testing/TestMyDate.java
+++ src/com/acme/ch08encapsulation/testing/TestMyDate.java
@@ -1,13 +1,21 @@
 package com.acme.ch08encapsulation.testing;
 
-import com.acme.ch06syntax.domain.MyDate;
+
+import com.acme.ch08encapsulation.domain.MyDate;
 
 public class TestMyDate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MyDate.leapYears();
-
+		MyDate d1 = new MyDate(13, 11, 2017);
+		System.out.println("Today is " + d1);
+		
+		MyDate d2 = new MyDate(32, 11, 2017);
+		System.out.println("Next day is " + d2);
+		
+		MyDate d3 = new MyDate(12, 32, 2017);
+		System.out.println("Next day is " + d3);
+		
+		
 	}
 
 }
