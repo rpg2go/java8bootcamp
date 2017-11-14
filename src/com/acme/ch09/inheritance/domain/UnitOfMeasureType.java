29648ade4a990d43df510bf654a4e5fb4cf787c5
Marius Druga <druga.marius@gmail.com>
2017-11-14 23:42:15 +0200

    chapter 9

---
+++ src/com/acme/ch09/inheritance/domain/UnitOfMeasureType.java
@@ -0,0 +1,5 @@
+package com.acme.ch09.inheritance.domain;
+
+public enum UnitOfMeasureType { 
+	LITTER, GALLON, CUBIC_METER, CUBIC_FEET
+}
---
+++ src/com/acme/ch09/inheritance/domain/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Marius
+ *
+ */
+package com.acme.ch09.inheritance.domain;
